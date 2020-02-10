c8f71e1a426bb5b891ca8a386f4f215820d6847d
Thomas Abplanalp <info@tasoft.ch>
2020-02-10 17:39:01 +0100

    Add communication between sps engines

---
+++ src/Communication/AbstractCommunication.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2019, TASoft Applications
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace Ikarus\SPS\Communication;
+
+
+use Ikarus\SPS\Exception\CommunicationException;
+use Ikarus\SPS\Exception\SocketException;
+use Ikarus\SPS\Exception\SPSException;
+use Throwable;
+
+abstract class AbstractCommunication implements CommunicationInterface
+{
+    private $timeout = 1.0;
+    const SOCK_BUFFER_SIZE = 2048;
+
+    /**
+     * AbstractCommunication constructor.
+     * @param float $timeout
+     * @param null $socket
+     */
+    public function __construct(float $timeout = 1.0, $socket = NULL)
+    {
+        $this->timeout = $timeout;
+        $this->socket = $socket;
+    }
+
+    /**
+     * This method must be able to establish a connection and return a socket to read and write.
+     *
+     * Called to establish the connection.
+     * @return resource
+     */
+    abstract protected function establishConnection();
+
+    /**
+     * Called after transaction to close the connection.
+     *
+     * @param $socket
+     */
+    abstract protected function closeConnection($socket);
+
+    /**
+     * Send silently to SPS
+     *
+     * @param $command
+     * @param $error
+     * @return string|null
+     */
+    public function sendSilentlyToSPS($command, &$error = NULL) {
+        try {
+            return $this->sendToSPS($command);
+        } catch (Throwable $exception) {
+            $error = $exception;
+        }
+        return NULL;
+    }
+
+    public function sendToSPS($command)
+    {
+        $socket = $this->establishConnection();
+        if(!is_resource($socket)) {
+            $e = new SocketException("No connected socket available");
+            $e->setSocket($socket);
+            throw $e;
+        }
+
+        if(($to = $this->getTimeout()) > 0.1 ) {
+            $sec = floor($to);
+            $usec = $to - $sec;
+
+            socket_set_option($socket,SOL_SOCKET, SO_RCVTIMEO, [
+                "sec"=>$sec,
+                "usec"=>$usec
+            ]);
+        }
+
+
+        $e = function() use ($socket) {
+            $error = socket_last_error($socket);
+            if($error) {
+                $e = new CommunicationException(socket_strerror( $error ), $error);
+                $e->setSocket($socket);
+                throw $e;
+            }
+            socket_clear_error($socket);
+        };
+
+        try {
+            $len = strlen($command);
+            $total = 0;
+
+            while ($written = socket_write($socket, $command)) {
+                if($written === false)
+                    break;
+                $total += $written;
+                if($total >= $len)
+                    break;
+            }
+
+            $e();
+
+            $buffer = "";
+
+            while ($out = socket_read($socket, static::SOCK_BUFFER_SIZE)) {
+                $buffer .= $out;
+                if(strlen($out) < static::SOCK_BUFFER_SIZE) {
+                    break;
+                }
+            }
+
+            $e();
+
+            return $buffer;
+        } catch (SPSException $exception) {
+            throw $exception;
+        } finally {
+            $this->closeConnection($socket);
+        }
+    }
+
+
+    /**
+     * @return float
+     */
+    public function getTimeout(): float
+    {
+        return $this->timeout;
+    }
+}
---
+++ src/Plugin/Intermediate/AbstractIntermediatePlugin.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2019, TASoft Applications
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace Ikarus\SPS\Plugin\Intermediate;
+
+
+use Ikarus\SPS\Plugin\AbstractPlugin;
+use Ikarus\SPS\Plugin\Management\PluginManagementInterface;
+use Ikarus\SPS\Plugin\TearDownPluginInterface;
+
+abstract class AbstractIntermediatePlugin extends AbstractPlugin implements IntermediatePluginInterface, TearDownPluginInterface
+{
+    /** @var string */
+    private $address;
+    /** @var int|null  */
+    private $port;
+    /** @var string */
+    private $startMessage;
+    /** @var resource */
+    protected $socket;
+
+    const SOCK_BACKLOG = 1;
+    const SOCK_BUFFER_SIZE = 2048;
+
+    /**
+     * AbstractIntermediatePlugin constructor.
+     * @param string $address
+     * @param int|null $port
+     * @param string $startMessage
+     */
+    public function __construct(string $address, int $port = NULL, string $startMessage = 'Welcome to Remote Event Server of Ikarus SPS!')
+    {
+        $this->address = $address;
+        $this->port = $port;
+        $this->startMessage = $startMessage;
+    }
+
+    /**
+     * This method should handle any incoming commands onto the SPS.
+     *
+     * @param $command
+     * @param PluginManagementInterface $management
+     * @return string
+     */
+    abstract protected function doCommand($command, PluginManagementInterface $management): string;
+
+    /**
+     * This method can be called to simply accept an incoming connection, read its request and sends the SPS response back.
+     *
+     * @param PluginManagementInterface $management
+     */
+    protected function trapNextCommand(PluginManagementInterface $management) {
+        $msgsock = socket_accept($this->socket);
+        if($msgsock) {
+            $buffer = "";
+
+            while ($out = socket_read($this->socket, static::SOCK_BUFFER_SIZE)) {
+                $buffer .= $out;
+                if(strlen($out) < static::SOCK_BUFFER_SIZE) {
+                    break;
+                }
+            }
+
+            $response = $this->doCommand($buffer, $management);
+
+            $len = strlen($response);
+            $total = 0;
+
+            while ($written = socket_write($msgsock, $response)) {
+                if($written === false)
+                    break;
+                $total += $written;
+                if($total >= $len)
+                    break;
+            }
+
+            socket_close($msgsock);
+            return $buffer;
+        }
+        return NULL;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPort(): ?int
+    {
+        return $this->port;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStartMessage(): string
+    {
+        return $this->startMessage;
+    }
+
+    protected function establishConnection() {
+        if(!$this->socket) {
+            if (($this->socket = $sock = socket_create(NULL !== $this->getPort() ? AF_INET : AF_UNIX, SOCK_STREAM, SOL_TCP)) === false) {
+                trigger_error( "socket_create() failed: " . socket_strerror(socket_last_error()), E_USER_WARNING);
+            }
+
+            if (socket_bind($sock, $this->address, $this->port) === false) {
+                trigger_error( "socket_bind() failed: " . socket_strerror(socket_last_error($sock)), E_USER_WARNING);
+            }
+
+            if (socket_listen($sock, static::SOCK_BACKLOG) === false) {
+                trigger_error( "socket_listen() failed: " . socket_strerror(socket_last_error($sock)), E_USER_WARNING);
+            }
+            socket_getsockname($sock, $this->address, $this->port);
+        }
+    }
+
+    public function tearDown()
+    {
+        if($this->socket)
+            socket_close($this->socket);
+        $this->socket = NULL;
+    }
+}
