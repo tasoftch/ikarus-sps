428704a86169e6a5cf2a12b161bf6c698ef34252
Thomas Abplanalp <info@tasoft.ch>
2020-02-20 14:24:49 +0100

    Create alerts

---
+++ src/Alert/AbstractAlert.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2019, TASoft Applications
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ *  Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+namespace Ikarus\SPS\Alert;
+
+
+use Ikarus\SPS\Plugin\PluginInterface;
+
+class AbstractAlert implements AlertInterface
+{
+    /** @var int */
+    private $code;
+    /** @var string */
+    private $message;
+    /** @var string|PluginInterface|null */
+    private $affectedPlugin;
+
+    /**
+     * AbstractAlert constructor.
+     * @param int $code
+     * @param string $message
+     * @param PluginInterface|string|null $affectedPlugin
+     * @param array $args
+     */
+    public function __construct(int $code, string $message, $affectedPlugin, ...$args)
+    {
+        $this->code = $code;
+        $this->message = vsprintf($message, $args);
+        $this->affectedPlugin = $affectedPlugin;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @return PluginInterface|string|null
+     */
+    public function getAffectedPlugin()
+    {
+        return $this->affectedPlugin;
+    }
+}
